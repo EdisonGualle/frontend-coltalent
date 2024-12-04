4d9f57db40156af769a4fc10644a042205723c1c
EdisonGualle <ed.gualle@gmail.com>
2024-12-04 05:27:59 -0500

    feat: Enhanced sidebar design with hover and active state differentiation for menus and submenus

--- src/components/common/layout/Sidebar.jsx
+++ src/components/common/layout/Sidebar.jsx
@@ -49,159 +49,182 @@ const Sidebar = () => {
   return (
     <>
       <div
-        className={`xl:h-[100vh] absolute overflow-y-scroll xl:static w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-secondary-900 p-4 flex flex-col justify-between z-50  ${showMenu ? "left-0" : "-left-full"
+        className={`xl:h-[100vh] absolute overflow-y-scroll xl:static w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-gray-300  flex flex-col justify-between z-50  ${showMenu ? "left-0" : "-left-full"
           } transition-all`}
       >
         <div>
-          <h1 className="text-center text-2xl font-bold text-primary mb-10">
-            COLTALENT<span className="text-secondary-100 text-4xl">.</span>
-          </h1>
-          <ul>
+          <div className="px-6 py-4 bg-gradient-to-r from-gray-800 to-gray-900 text-white rounded-br-3xl shadow-md sticky top-0 z-10">
+            <h1 className="text-2xl font-bold">COLTALENT</h1>
+            <p className="text-sm opacity-75">Gestión Eficiente de RRHH</p>
+          </div>
+
+          {/* Menu */}
+          <ul className="flex-1 space-y-2 px-4 py-8">
+
+            {/* Separador */}
+            <hr className="pb-2" />
+
+            {/* Tablero */}
             <li className="group">
               <Link
-                to=""
-                className="flex text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                to="/"
+                className="w-full flex text-gray-800 items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-600 hover:shadow-sm  transition-colors"
               >
-                <RiDashboardLine className="text-primary text-xl group-hover:text-yellow-500" /> Tablero
+                <RiDashboardLine className="text-gray-800 text-xl group-hover:text-blue-700" />
+                <span className="font-medium">Tablero</span>
               </Link>
             </li>
 
-            {/* Todos */}
+            {/* Mi Perfil - Acceso Todos*/}
             <li className="group">
               <button
                 onClick={handleProfileClick}
-                className=" w-full flex text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                className="w-full flex text-gray-800 items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm  transition-colors"
               >
-                <RiUser3Line className="text-primary text-xl group-hover:text-yellow-500" /> Mi Perfil
+                <RiUser3Line className="text-gray-800 text-xl group-hover:text-blue-700" />
+                <span className="font-medium">Mi Perfil</span>
               </button>
             </li>
 
-
+            {/* Empleados - Acceso Administrador*/}
             {(userRole === 'Administrador') && (
               <li className="group">
                 <Link
                   to="/empleados"
-                  className="flex text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                  className="w-full flex text-gray-800 items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm  transition-colors"
                 >
-                  <RiUserStarLine className="text-primary text-xl group-hover:text-yellow-500" /> Empleados
+                  <RiUserStarLine className="text-gray-800 text-xl group-hover:text-blue-700" />
+                  <span className="font-medium">Empleados</span>
                 </Link>
               </li>
             )}
 
-
+            {/* Usuarios - Acceso Administrador*/}
             {(userRole === 'Administrador') && (
               <li className="group">
                 <Link
                   to="/usuarios"
-                  className="flex text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                  className="w-full flex text-gray-800 items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm  transition-colors"
                 >
-                  <RiUser3Line className="text-primary text-xl group-hover:text-yellow-500" /> Usuarios
+                  <RiUser3Line className="text-gray-800 text-xl group-hover:text-blue-700" />
+                  <span className="font-medium">Usuarios</span>
                 </Link>
               </li>
             )}
 
-
-            <li className="group">
+            {/* Permisos */}
+            <li >
               <button
                 onClick={() => toggleSubMenu(3)}
-                className="w-full flex text-secondary-100 items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                className="group w-full flex text-gray-800 items-center justify-between py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm transition-colors"
               >
                 <span className="flex items-center gap-4">
-                  <RiShieldUserLine className="text-primary text-xl group-hover:text-yellow-500" />{" "}
-                  Permisos
+                  <RiShieldUserLine className="text-gray-800 text-xl group-hover:text-blue-700" />{" "}
+                  <span className="font-medium">Permisos</span>
                 </span>
                 <RiArrowRightSLine
                   className={`mt-1 ${activeSubmenu === 3 && "rotate-90"} transition-all`}
                 />
               </button>
+
+              {/* Solicitudes - Acceso Todos */}
               <ul
-                className={` ${activeSubmenu === 3 ? "h-auto" : "h-0"} overflow-y-hidden transition-all`}
+                className={`mt-2 space-y-1 pl-8 overflow-hidden transition-all ${activeSubmenu === 3 ? "h-auto" : "h-0"
+                  }`}
               >
                 <li>
                   <button
-                     onClick={handlePermisosClick}
-                    className="flex items-center gap-2 py-1 text-secondary-100 px-4 ml-6  relative hover:text-primary transition-colors"
+                    onClick={handlePermisosClick}
+                    className="w-full flex items-start  py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
                   >
-                    <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
                     Solicitudes
                   </button>
                 </li>
+
+                {/* Autorizaciones - Acceso Administrador, Jefe Dirección, Jefe Unidad, Jefe General */}
                 {(userRole === 'Administrador' || userRole === 'Jefe Dirección' || userRole === 'Jefe Unidad' || userRole === 'Jefe General') && (
                   <li>
                     <Link
                       to="/permisos/autorizaciones"
-                      className="flex items-center gap-2 py-1 text-secondary-100 px-4 ml-6  relative hover:text-primary transition-colors"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
                     >
-                      <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
                       Autorizaciones
                     </Link>
                   </li>
                 )}
-                  {(userRole === 'Administrador') && (
-                <li>
-                  <Link
-                    to="/permisos/tipos"
-                    className="flex items-center gap-2 py-1 text-secondary-100 px-4 ml-6  relative hover:text-primary transition-colors"
-                  >
-                    <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
-                    Tipos de Permisos
-                  </Link>
-                </li>
+
+                {/* Tipos de Permisos - Acceso Administrador */}
+                {(userRole === 'Administrador') && (
+                  <li>
+                    <Link
+                      to="/permisos/tipos"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
+                    >
+                      Tipos de Permisos
+                    </Link>
+                  </li>
                 )}
-                  {(userRole === 'Administrador') && (
-                <li>
-                  <Link
-                    to="/permisos/motivos-rechazo"
-                    className="flex items-center gap-2 py-1 text-secondary-100 px-4 ml-6  relative hover:text-primary transition-colors"
-                  >
-                    <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
-                    Motivos de Rechazo
-                  </Link>
-                </li>
+
+                {/* Motivos de Rechazo - Acceso Administrador */}
+                {(userRole === 'Administrador') && (
+                  <li>
+                    <Link
+                      to="/permisos/motivos-rechazo"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
+                    >
+                      Motivos de Rechazo
+                    </Link>
+                  </li>
                 )}
               </ul>
             </li>
-            {(userRole === 'Administrador' ) && (
-              <li className="group">
+
+            {/* Organización - Acceso Administrador*/}
+            {(userRole === 'Administrador') && (
+              <li>
                 <button
                   onClick={() => toggleSubMenu(1)}
-                  className="w-full flex text-secondary-100 items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                  className="group w-full flex text-gray-800 items-center justify-between py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm transition-colors"
                 >
                   <span className="flex items-center gap-4">
-                    <HiOutlineOfficeBuilding className="text-primary text-xl group-hover:text-yellow-500" />{" "}
-                    Organización
+                    <HiOutlineOfficeBuilding className="text-gray-800 text-xl group-hover:text-blue-700" />{" "}
+                    <span className="font-medium">Organización</span>
                   </span>
                   <RiArrowRightSLine
-                    className={`mt-1 ${activeSubmenu === 1 && "rotate-90"} transition-all`}
+                    className={`mt-2 ${activeSubmenu === 1 && "rotate-90"} transition-all`}
                   />
                 </button>
                 <ul
-                  className={` ${activeSubmenu === 1 ? "h-auto" : "h-0"} overflow-y-hidden transition-all`}
+                  className={`mt-2 space-y-1 pl-8 overflow-hidden transition-all ${activeSubmenu === 1 ? "h-auto" : "h-0"
+                    }`}
                 >
+
+                  {/* Direcciones - Acceso Administrador */}
                   <li>
                     <Link
                       to="/direcciones"
-                      className="flex items-center gap-2 py-1 text-secondary-100 px-4 ml-6  relative hover:text-primary transition-colors"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
                     >
-                      <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
                       Direcciones
                     </Link>
                   </li>
+
+                  {/* Unidades - Acceso Administrador */}
                   <li>
                     <Link
                       to="/unidades"
-                      className="flex items-center gap-2 py-1 text-secondary-100 px-4 ps-7 ml-6  relative hover:text-primary transition-colors"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
                     >
-                      <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
                       Unidades
                     </Link>
                   </li>
+
+                  {/* Cargos - Acceso Administrador */}
                   <li>
                     <Link
                       to="/cargos"
-                      className="flex items-center gap-2 py-1 text-secondary-100 px-4 ml-6  relative hover:text-primary transition-colors"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 hover:shadow-md hover:text-blue-700 rounded-lg transition-all"
                     >
-                      <RiArrowDropRightLine className="text-primary text-xl group-hover:text-yellow-500 inline-block" />
                       Cargos
                     </Link>
                   </li>
@@ -209,23 +232,26 @@ const Sidebar = () => {
               </li>
             )}
 
+            {/* Configuraciones - Acceso Administrador */}
             {(userRole === 'Administrador') && (
               <li className="group">
                 <Link
                   to="/configuraciones"
-                  className="flex text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                  className="w-full flex text-gray-800 items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm  transition-colors"
                 >
-                  <RiSettings3Line className="text-primary text-xl group-hover:text-yellow-500" />
-                  Configuraciones
+                  <RiSettings3Line className="text-gray-800 text-xl group-hover:text-blue-700" />
+                  <span className="font-medium">Configuraciones</span>
                 </Link>
               </li>
             )}
           </ul>
         </div>
-        <nav className="group">
-          <button onClick={handleLogout} className="flex w-full text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors">
-            <RiLogoutCircleRLine className="text-primary text-xl group-hover:text-yellow-500" /> Cerrar
-            sesión
+
+        <nav className="p-4">
+          <hr className="mb-4" />
+          <button onClick={handleLogout} className="group w-full flex text-gray-800 items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-100 hover:text-blue-700 hover:shadow-sm  transition-colors">
+            <RiLogoutCircleRLine className="text-gray-800 text-xl group-hover:text-blue-700" />
+            <span className="font-medium">Cerrar sesión</span >
           </button>
         </nav>
       </div>
